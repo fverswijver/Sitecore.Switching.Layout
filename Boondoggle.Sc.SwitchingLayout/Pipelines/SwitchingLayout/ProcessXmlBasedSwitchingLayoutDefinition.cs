71b5bf9858559fc4c10f4808db5f9b12409815bd
Filip Verswijver <filip.verswijver@boondoggle.eu>
2016-09-27 11:45:04 +0200

    - Added project - Added SC nuget references - Added gitignore

---
+++ Boondoggle.Sc.SwitchingLayout/Pipelines/SwitchingLayout/ProcessXmlBasedSwitchingLayoutDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Pipelines.Response.BuildPageDefinition;
+using Sitecore.Mvc.Presentation;
+
+namespace Boondoggle.Sc.Pipelines.SwitchingLayout
+{
+    public class ProcessXmlBasedSwitchingLayoutDefinition : ProcessXmlBasedLayoutDefinition
+    {
+        protected override Rendering GetRendering(XElement renderingNode, Guid deviceId, Guid layoutId, string renderingType, XmlBasedRenderingParser parser)
+        {
+            var rendering = base.GetRendering(renderingNode, deviceId, layoutId, renderingType, parser);
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(renderingType) && renderingType == "Layout")
+                {
+                    if (HttpContext.Current.Items.Contains("SwitchingLayoutGuid"))
+                    {
+                        var switchingLayoutId = (Guid)HttpContext.Current.Items["SwitchingLayoutGuid"];
+
+                        Tracer.Info("Setting XmlBasedLayoutDefinition SwitchingLayout to " + switchingLayoutId.ToString());
+                        rendering.LayoutId = switchingLayoutId;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Tracer.Error("Unable to process XmlBasedLayoutDefinition SwitchingLayout");
+            }
+
+            return rendering;
+        }
+    }
+}
---
+++ Boondoggle.Sc.SwitchingLayout/Pipelines/SwitchingLayout/SwitchingLayoutResolver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Web;
+
+namespace Boondoggle.Sc.Pipelines.SwitchingLayout
+{
+    public class SwitchingLayoutResolver : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            if (Sitecore.Context.Item == null)
+                return;
+
+            var layoutItem = GetQueryStringLayout() ?? Sitecore.Context.Item.Visualization.Layout;
+            if (layoutItem != null && layoutItem.InnerItem.TemplateID.ToString() == "{2C87C6FE-F615-4E92-B321-1BAE3D488B0D}")
+            {
+                Tracer.Info("Found SwitchingLayout");
+
+                ReferenceField layoutField = null;
+                if (Sitecore.Context.PageMode.IsExperienceEditor)
+                {
+                    layoutField = (ReferenceField)layoutItem.InnerItem.Fields["EditingLayout"];
+                }
+                else
+                {
+                    layoutField = (ReferenceField)layoutItem.InnerItem.Fields["NormalLayout"];
+                }
+
+                if (layoutField != null)
+                {
+                    var specificLayoutItem = layoutField.TargetItem;
+                    if (specificLayoutItem != null)
+                    {
+                        Tracer.Info(string.Format("[{0}] Setting SwitchingLayout to {1}", Sitecore.Context.PageMode.IsExperienceEditor ? "Editing" : "Normal", specificLayoutItem.ID.ToString()));
+
+                        var newLayout = new LayoutItem(specificLayoutItem);
+                        Sitecore.Context.Page.FilePath = newLayout.FilePath;
+                        args.Context.Items.Add("SwitchingLayoutGuid", specificLayoutItem.ID.ToGuid());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the layout.
+        /// 
+        /// </summary>
+        /// 
+        /// <returns>
+        /// The layout.
+        /// </returns>
+        private static LayoutItem GetQueryStringLayout()
+        {
+            string queryString = WebUtil.GetQueryString("sc_layout");
+            if (string.IsNullOrEmpty(queryString))
+                return null;
+
+            return Sitecore.Context.Database.GetItem(queryString);
+        }
+    }
+}
